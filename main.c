76b50cabfbfa806379fe9a1a58b5ed3950f3635b
saifmohsen105 <168905609+saifmohsen105@users.noreply.github.com>
2024-05-06 17:10:21 +0300

    .

--- main.c
+++ main.c
@@ -1,95 +1,36 @@
 #include <stdio.h>
-#include <string.h>
-int main(){
-int row_1,colum_1,row_2,colum_2,i,j,queation;
-printf("Please write row matrix 1 - > ");
-scanf("%i",&row_1);
-printf("Please write colum matrix 1 - > ");
-scanf("%i",&colum_1);
-printf("Please write row matrix 2 - > ");
-scanf("%i",&row_2);
-printf("Please write colum matrix 2 - > ");
-scanf("%i",&colum_2);
-int matrix_1[row_1][colum_1],matrix_2[row_2][colum_2];
-for(i = 0;i<colum_1;i++){
-    for(j = 0;j<row_1;j++){
-        printf("Please write matrix 1 (%i,%i) - > ",j+1,i);
-        scanf("%i",&matrix_1[j][i]);
-    }
-}
-printf("-------------------------------- \n");
-for(i = 0;i<colum_2;i++){
-    for(j = 0;j<row_2;j++){
-        printf("Please write matrix 2 (%i,%i) - > ",j+1,i);
-        scanf("%i",&matrix_2[j][i]);
-    }
-}
-printf("-------------------------------- \n");
-for(i = 0;i<colum_1;i++){
-    for(j = 0;j<row_1;j++){
-        printf(" matrix 1 (%i,%i) = %i \t ",j+1,i,matrix_1[j][i]);
-    }
-    printf("\n");
-}
-printf("-------------------------------- \n");
-for(i = 0;i<colum_2;i++){
-    for(j = 0;j<row_2;j++){
-        printf(" matrix 2 (%i,%i) = %i \t ",j+1,i,matrix_2[j][i]);
-    }
-    printf("\n");
-}
-printf("-------------------------------- \n");
-printf("Do u want mult , sub , sum (1,2,3) - > ");
-scanf("%i",&queation);
-int mul[100][100];
-switch(queation){
-case 1:
-    if(colum_1!=row_2){
-    printf("I can not multiption matrix...!");
-    return  0;
-}
-for ( i = 0; i < row_1; i++) {
-    for ( j = 0; j < colum_2; j++) {
-      for (int k = 0; k < colum_1; k++) {
-        mul[i][j] += matrix_1[i][k] * matrix_2[k][j];
-      }
-    }
-  }
-for(i = 0;i<colum_2;i++){
-    for(j = 0;j<row_1;j++){
-        printf("%i\t",mul[j][i]);
-    }
-    printf("\n");
-}
-break;
-case 2 :
-   for(i = 0;i<row_1;i++){
-        for(j = 0;j<row_2;j++){
-           mul[i][j] += matrix_1[i][j] + matrix_2[i][j];
-       }
+#include <stdlib.h>
+#include<math.h>
+int main()
+{
+   int num_real,num_not_real,degree;
+   double seta;
+   const double PI = 3.14159265;
+   printf("Please write real number - > ");
+   scanf("%i",&num_real);
+   printf("Please write not real number - > ");
+   scanf("%i",&num_not_real);
+   int r = (num_real*num_real+num_not_real*num_not_real)/2;
+   seta = atan(num_not_real/num_real);
+   degree = (seta*180)/PI;
+   if(degree<0){
+    degree*=-1;
    }
-       for(i = 0;i<colum_2;i++){
-            for(j = 0;j<row_1;j++){
-               printf("%i\t",mul[j][i]);
-        }
-    printf("\n");
-    }
-    break;
-case 3:
-   for(i = 0;i<row_1;i++){
-        for(j = 0;j<row_2;j++){
-           mul[i][j] += matrix_1[i][j] - matrix_2[i][j];
-       }
+   if(num_real>0&&num_not_real>0){
+    seta = degree;
+   }else if (num_not_real<0&&num_real>0){
+       seta = 180-degree;
+   }else if (num_not_real<0&&num_real<0){
+   seta = 180+degree;
+   }else if (num_real>0&&num_not_real<0){
+   seta = 360-degree;
+   }else if (num_real==0&&num_not_real >0){
+   seta = 90;
+   }else if (num_real<0&&num_not_real ==0){
+   seta = 0;
+   }else if (num_real==0&&num_not_real<0||&num_not_real>0){
+   seta = 270;
    }
-       for(i = 0;i<colum_2;i++){
-            for(j = 0;j<row_1;j++){
-               printf("%i\t",mul[j][i]);
-        }
-    printf("\n");
-    }
-    break;
-default:
-    printf("^__Error__^");
-}
-	return 0;
+   printf("%i*(cos(%f)+i*sin(%f))",r,seta,seta);
+    return 0;
 }
